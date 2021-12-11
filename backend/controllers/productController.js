0bf0b0f14a18a3023b66571dc8ac81d00310e792
Thunderer0 <parilsanghvi0510@gmail.com>
2021-12-11 20:55:09 +0530

    added working model of product and updated routes,controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,20 @@
+const Product = require("../models/productModel")
+
+
+
+
+
+// create product
+exports.createProduct = async (req,res,next)=>{
+    
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+
 exports.getAllProducts = (req,res)=>{
     res.status(200).json({message:"Route is workig fine"})
 }
