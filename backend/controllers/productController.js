22203fac80604db4fe15d8089a42b5a94798ced1
Thunderer0 <parilsanghvi0510@gmail.com>
2021-12-13 17:47:46 +0530

    added features of search,filter and pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,12 +1,13 @@
 // takes model for object insertion from prodectmodel
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utlis/errorhandler");
-const catchAsyncErrors = require("../middleware/catchAsyncErrors")
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Apifeatures = require("../utlis/apifeatures");
 
 
 // create product --admin
 // exports.(function_name) = rest of function {way to export functions ;) }
-exports.createProduct =catchAsyncErrors(async (req, res, next) => {  
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
     // takes json as input and adds to database according to schema
     // Product.create/find are operations on database with help of mongoose.export taken in Product
     const product = await Product.create(req.body);
@@ -19,11 +20,19 @@ exports.createProduct =catchAsyncErrors(async (req, res, next) => {
 
 // get all products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+    const resultPerPage = 5
+    const productCount = await Product.countDocuments();
+
+    const apifeature = new Apifeatures(Product.find(), req.query)
+        .search()
+        .filter()
+        .pagiNation(resultPerPage)
     // find function gets all the objects of the collection returns empty object if none is found
-    const products = await Product.find()
+    const products = await apifeature.query;
     res.status(200).json({
         success: true,
-        products
+        products,
+        productCount
     })
 })
 // update product --admin
@@ -31,7 +40,7 @@ exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
     // takes json and product id as input finds object by that id if such object exists it again finds by id and updates it and returns success with modified object
     let product = await Product.findById(req.params.id);
     if (!product) {
-        return next(new ErrorHandler("product not found",404))
+        return next(new ErrorHandler("product not found", 404))
     }
     product = await Product.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
@@ -43,24 +52,24 @@ exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
     })
 })
 // delete product --admin
-exports.deleteProduct = catchAsyncErrors(async (req, res,next) => {
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
     // takes product id finds that id and deletes the object
     const product = await Product.findById(req.params.id)
     if (!product) {
-        return next(new ErrorHandler("product not found",404))
+        return next(new ErrorHandler("product not found", 404))
     }
     await product.remove();
     res.status(200).json({
         success: true,
-        message:"product deleted"
+        message: "product deleted"
     })
 })
 // get one product
-exports.getProductDetails = catchAsyncErrors(async (req,res,next)=>{
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
     // takes id as input finds it and if such object exists it returns object 
     const product = await Product.findById(req.params.id)
     if (!product) {
-        return next(new ErrorHandler("product not found",404))
+        return next(new ErrorHandler("product not found", 404))
     }
     res.status(200).json({
         success: true,
---
+++ backend/utlis/apifeatures.js
@@ -0,0 +1,43 @@
+const {
+    json
+} = require("express/lib/response");
+
+class Apifeatures {
+    constructor(query, querystr) {
+        this.query = query
+        this.querystr = querystr
+    }
+    search() {
+        const keyword = this.querystr.keyword ? {
+            name: {
+                $regex: this.querystr.keyword,
+                $options: "i",
+            }
+        } : {}
+        this.query = this.query.find({
+            ...keyword
+        })
+        return this;
+    }
+    filter() {
+        const queryCopy = {
+            ...this.querystr
+        }
+        // remove some fields for category
+        const removeFields = ["keyword", "page", "limit"]
+        removeFields.forEach(key => delete queryCopy[key])
+        // filter for price and rating
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, key => `$${key}`)
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+    pagiNation(resultPerPage) {
+        const currentPage = Number(this.querystr.page) || 1
+        const skip = resultPerPage * (currentPage - 1)
+        this.query = this.query.limit(resultPerPage).skip(skip)
+        return this;
+    }
+}
+module.exports = Apifeatures
