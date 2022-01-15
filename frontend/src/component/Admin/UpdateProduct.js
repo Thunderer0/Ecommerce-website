5d875a12cb91f8e8f19d05539ebe8d4a18f6d6ea
Thunderer0 <parilsanghvi0510@gmail.com>
2022-01-16 00:29:11 +0530

    make it better

--- frontend/src/component/Admin/UpdateProduct.js
+++ frontend/src/component/Admin/UpdateProduct.js
@@ -32,7 +32,7 @@ const UpdateProduct = ({ history, match }) => {
   const [price, setPrice] = useState(0);
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
-  const [Stock, setStock] = useState(0);
+  const [stock, setStock] = useState(0);
   const [images, setImages] = useState([]);
   const [oldImages, setOldImages] = useState([]);
   const [imagesPreview, setImagesPreview] = useState([]);
@@ -57,7 +57,7 @@ const UpdateProduct = ({ history, match }) => {
       setDescription(product.description);
       setPrice(product.price);
       setCategory(product.category);
-      setStock(product.Stock);
+      setStock(product.stock);
       setOldImages(product.images);
     }
     if (error) {
@@ -95,7 +95,7 @@ const UpdateProduct = ({ history, match }) => {
     myForm.set("price", price);
     myForm.set("description", description);
     myForm.set("category", category);
-    myForm.set("Stock", Stock);
+    myForm.set("stock", stock);
 
     images.forEach((image) => {
       myForm.append("images", image);
@@ -192,7 +192,7 @@ const UpdateProduct = ({ history, match }) => {
                 placeholder="Stock"
                 required
                 onChange={(e) => setStock(e.target.value)}
-                value={Stock}
+                value={stock}
               />
             </div>
 
