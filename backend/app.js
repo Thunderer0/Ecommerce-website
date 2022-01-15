9ccc7046a101c695b52194d6ebc27de89e1431a3
Thunderer0 <parilsanghvi0510@gmail.com>
2022-01-15 14:33:16 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const errorMidddleware = require('./middleware/error');
 const bodyParser = require('body-parser')
 const fileUpload = require("express-fileupload")
 const dotenv = require("dotenv")
+const path = require("path")
 // config
 dotenv.config({path:"backend/config/config.env"})
 // parsing object to json
@@ -19,12 +20,20 @@ const product = require("./routes/productRoute");
 const user = require("./routes/userRoute")
 const order = require("./routes/orderRoute")
 const payment = require("./routes/paymentRoute")
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/config.env" });
+  }
 // it will give route to product crud operations
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
+app.use(express.static(path.join(__dirname,"../frontend/public")))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/public/index.html"));
+  });
 // middleware for error
 app.use(errorMidddleware);
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 // import app
 const app = require("./app")
-const dotenv = require("dotenv")
 const cloudinary = require("cloudinary")
 const connectDatabase = require('./config/database')
 // handeling uncaught exception
@@ -12,7 +11,9 @@ process.on("uncaughtException",(err)=>{
     })
 })
 // config
-dotenv.config({path:"backend/config/config.env"})
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "backend/config/config.env" });
+  }
 
 //  connect database
 connectDatabase()
