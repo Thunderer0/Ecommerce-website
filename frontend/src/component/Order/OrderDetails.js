63493effba207ae11227daf011c070627288400c
Thunderer0 <parilsanghvi0510@gmail.com>
2022-01-15 22:03:43 +0530

    make it better

--- frontend/src/component/Order/OrderDetails.js
+++ frontend/src/component/Order/OrderDetails.js
@@ -5,7 +5,7 @@ import MetaData from "../layout/MetaData";
 import { Link } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 import { getOrderDetails, clearErrors } from "../../actions/orderAction";
-import Loader from "../layout/Loader/Loader";
+// import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
 
 const OrderDetails = ({ match }) => {
@@ -24,9 +24,9 @@ const OrderDetails = ({ match }) => {
   }, [dispatch, alert, error, match.params.id]);
   return (
     <Fragment>
-      {loading ? (
+      {/* {loading ? (
         <Loader />
-      ) : (
+      ) : ( */}
         <Fragment>
           <MetaData title="Order Details" />
           <div className="orderDetailsPage">
@@ -114,7 +114,8 @@ const OrderDetails = ({ match }) => {
             </div>
           </div>
         </Fragment>
-      )}
+      )
+      {/* } */}
     </Fragment>
   );
 };
